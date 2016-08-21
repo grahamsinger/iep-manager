24a279e6950e4ab854daf98ed94340024dcf07c2
Graham Singer <grahamtsinger@gmail.com>
2016-08-21 16:01:33 -0700

    Good Commit

---
+++ app/js/app.js
@@ -0,0 +1,9 @@
+define(['jquery', 'app/js/caseload'], function($, CaseLoad){
+
+  $('#testing').text("Jane Hourigan!");
+
+  if(CaseLoad){
+    alert("CaseLoad Works, I guess");
+  }
+
+});
---
+++ app/js/caseload.js
@@ -0,0 +1,3 @@
+define(['underscore'], function(_){
+  console.log(_.uniq([1,1,2,2,2,2,2,2,3,4,5]));
+});
